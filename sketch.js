41a5280928aa388f0ec21445a60c5666b9306683
divya101108 <95427255+divya101108@users.noreply.github.com>
2022-01-11 18:01:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var path,pathing,runner,runnering
+
+function preload(){
+  //pre-load images
+  pathing=loadImage
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+}
+
+function draw() {
+  background(0);
+
+}
